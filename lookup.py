bf418e8107d307a01e221ae23d2ee5337c8cc272
junjinxiao <29254355+junjinxiao@users.noreply.github.com>
2019-02-23 21:05:27 +0800

    add lookup.py

--- lookup.py
+++ lookup.py
@@ -13,4 +13,11 @@ class Solution():
                 loopup[num] = i
 rlist = Solution().twoSum([2,7,11,15,3],10)
 print(rlist)
-print(loopup[2])
+#print(loopup[2])
+def main():
+    lsit = Solution().twoSum([2,7,11,-2,4,5],9)
+    print(lsit)
+
+if __name__ == '__main__':
+    main()
+
